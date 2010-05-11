f61ec7168e6155e78bfefe839aa713e5a9a65dce
Your Name <tylermack5@gmail.com>
2010-05-10 19:34:29 -0700

    Added regex validation to email field. Minor formatting changes- also, on last commit I added http basic authentication to all methods except for :create and :index

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,5 +4,5 @@ class Quote < ActiveRecord::Base
 	validates_numericality_of :student_id, :only_integer => true, :on => :create
 	validates_length_of :student_id, :is => 6, :message => "ID is the wrong length (should be 6 characters)"
 	validates_uniqueness_of :student_id, :scope => :student_id, :message => "ID has already been used"
-
+	validates_format_of :email, :with => /^.+@.+\.(com|org|net|edu)$/
 end
