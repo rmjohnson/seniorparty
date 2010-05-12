f03b627467fd8e10650a66d478563d5421835918
Ryan Johnson <kritstov@gmail.com>
2010-05-12 10:03:07 -0700

    Made an attempt at preventing quotes from showing up twice in a row on billboard.

--- app/controllers/billboard_controller.rb
+++ app/controllers/billboard_controller.rb
@@ -1,11 +1,14 @@
 class BillboardController < ApplicationController
   def index
+	prev = params[:id]
 	quotes = Quote.find(:all)
 	random = rand(quotes.length)
+	while  prev == random
+		random = rand(quotes.length)
+	end
 	@quote = Quote.find(quotes[random])
 	respond_to do |format|
 		format.html
 	end
   end
-
 end
